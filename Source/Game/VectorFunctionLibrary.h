dbedffb7d9d18ecc7b00987cbb285ea24aa9cb8c
danieljgerhardt <danieljgerhardt@gmail.com>
2025-09-02 13:13:18 -0400

    improved telekinesis logic

--- Source/Game/VectorFunctionLibrary.h
+++ Source/Game/VectorFunctionLibrary.h
@@ -14,8 +14,11 @@ class GAME_API UVectorFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
-	UFUNCTION(BlueprintPure, Category="Vector Math")
+	UFUNCTION(BlueprintPure, Category = "Vector Math")
 	static float getDistance(const FVector a, const FVector b);
+
+	UFUNCTION(BlueprintCallable, Category = "Vector Math")
+	float getDistanceCallable(const FVector a, const FVector b) { return getDistance(a, b); }
 
 	UFUNCTION(BlueprintPure, Category = "Vector Math")
 	static FVector getPointAtLookDistance(const FVector lookDirection, const FVector lookLocation, const float holderToObjectDistance, const FVector objectLocation);
